02583e23664fc463bd6e252165d673f71a1b795f
Viswanathamnarahari <viswanatham@gmail.com>
2025-12-20 09:47:05 -0500

    .rules file created

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -87,7 +87,7 @@ const Menu: React.FC<MenuProps> = ({
           <p className="text-blue-400 font-bold tracking-widest text-[10px] uppercase">For grand kids</p>
           <div className="w-8 h-px bg-slate-800 my-4" />
           <p className="text-slate-400 text-xs font-medium">Viswanatham@gmail.com</p>
-          <p className="text-slate-600 text-[10px] font-black tracking-[0.2em] mt-8 uppercase">251219.6</p>
+          <p className="text-slate-600 text-[10px] font-black tracking-[0.2em] mt-8 uppercase">251220.1</p>
       </div>
       
       <button 
